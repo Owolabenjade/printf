468d6670c39829c51e73a747850fac91010ab67f
Owolabenjade <obeejay99@gmail.com>
2023-11-14 15:58:13 -0800

    benjamin3

--- printf.c
+++ printf.c
@@ -71,7 +71,7 @@ int _printf(const char *format, ...)
 																																																																											                        }
 																																																								                }
 																																												                    break;
-																																														                case 'b':
+																																														                case 'u':
 																																														                    num = va_arg(args, unsigned int);
 																																																                    if (num == 0)
 																																																			                    {
@@ -80,32 +80,106 @@ int _printf(const char *format, ...)
 																																																											                    }
 																																																		                    else
 																																																					                    {
-																																																								                        int binary[32];
-																																																											                    int i = 0;
-
-																																																													                        while (num > 0)
+																																																								                        int divisor = 1;
+																																																											                    while (num / divisor != 0)
+																																																														                            divisor *= 10;
+																																																													                        while (divisor > 1)
 																																																																	                    {
-																																																																				                            binary[i++] = num % 2;
-																																																																							                            num /= 2;
-																																																																										                        }
-
-																																																																                    while (--i >= 0)
-																																																																			                        {
-																																																																							                        c = binary[i] + '0';
-																																																																										                        write(1, &c, 1);
-																																																																													                        count++;
-																																																																																                    }
-																																																																		                    }
+																																																																				                            divisor /= 10;
+																																																																							                            c = (num / divisor) + '0';
+																																																																										                            write(1, &c, 1);
+																																																																													                            count++;
+																																																																																                            num %= divisor;
+																																																																																			                        }
+																																																																                }
 																																																				                    break;
-																																																						                case '%':
-																																																						                    write(1, "%", 1);
-																																																								                    count++;
-																																																										                    break;
-																																																												                default:
-																																																												                    write(1, "%", 1);
-																																																														                    write(1, &(*ptr), 1);
-																																																																                    count += 2;
-																																																																		                }
+																																																						                case 'o':
+																																																						                    num = va_arg(args, unsigned int);
+																																																								                    if (num == 0)
+																																																											                    {
+																																																														                        write(1, "0", 1);
+																																																																	                    count++;
+																																																																			                    }
+																																																										                    else
+																																																													                    {
+																																																																                        int octal[32];
+																																																																			                    int i = 0;
+
+																																																																					                        while (num > 0)
+																																																																									                    {
+																																																																												                            octal[i++] = num % 8;
+																																																																															                            num /= 8;
+																																																																																		                        }
+
+																																																																								                    while (--i >= 0)
+																																																																											                        {
+																																																																															                        c = octal[i] + '0';
+																																																																																		                        write(1, &c, 1);
+																																																																																					                        count++;
+																																																																																								                    }
+																																																																										                    }
+																																																												                    break;
+																																																														                case 'x':
+																																																														                    num = va_arg(args, unsigned int);
+																																																																                    if (num == 0)
+																																																																			                    {
+																																																																						                        write(1, "0", 1);
+																																																																									                    count++;
+																																																																											                    }
+																																																																		                    else
+																																																																					                    {
+																																																																								                        int hex[32];
+																																																																											                    int i = 0;
+
+																																																																													                        while (num > 0)
+																																																																																	                    {
+																																																																																				                            hex[i++] = num % 16;
+																																																																																							                            num /= 16;
+																																																																																										                        }
+
+																																																																																                    while (--i >= 0)
+																																																																																			                        {
+																																																																																							                        c = (hex[i] < 10) ? hex[i] + '0' : hex[i] - 10 + 'a';
+																																																																																										                        write(1, &c, 1);
+																																																																																													                        count++;
+																																																																																																                    }
+																																																																																		                    }
+																																																																				                    break;
+																																																																						                case 'X':
+																																																																						                    num = va_arg(args, unsigned int);
+																																																																								                    if (num == 0)
+																																																																											                    {
+																																																																														                        write(1, "0", 1);
+																																																																																	                    count++;
+																																																																																			                    }
+																																																																										                    else
+																																																																													                    {
+																																																																																                        int hex[32];
+																																																																																			                    int i = 0;
+
+																																																																																					                        while (num > 0)
+																																																																																									                    {
+																																																																																												                            hex[i++] = num % 16;
+																																																																																															                            num /= 16;
+																																																																																																		                        }
+
+																																																																																								                    while (--i >= 0)
+																																																																																											                        {
+																																																																																															                        c = (hex[i] < 10) ? hex[i] + '0' : hex[i] - 10 + 'A';
+																																																																																																		                        write(1, &c, 1);
+																																																																																																					                        count++;
+																																																																																																								                    }
+																																																																																										                    }
+																																																																												                    break;
+																																																																														                case '%':
+																																																																														                    write(1, "%", 1);
+																																																																																                    count++;
+																																																																																		                    break;
+																																																																																				                default:
+																																																																																				                    write(1, "%", 1);
+																																																																																						                    write(1, &(*ptr), 1);
+																																																																																								                    count += 2;
+																																																																																										                }
 														            }
 								            else
 										            {
@@ -116,5 +190,5 @@ int _printf(const char *format, ...)
 
 					    va_end(args);
 
-					        return count;
+					        return (count);
 }
